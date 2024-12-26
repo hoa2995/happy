30b5f5e25d710d1dd36b001e09aab107e762b42b
hoa2995 <96247966+hoa2995@users.noreply.github.com>
2024-12-26 12:11:10 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'nov 25 2024',
-  name: 'Trần Đức Anh (chó vàng)'
+  birthdate: 'dec 26 2024',
+  name: 'Hưng Nghía'
 };
 
 function hideEverything() {
